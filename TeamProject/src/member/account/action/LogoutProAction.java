18cc42ee04a7a0f7d510c0bfff88e056ae89282d
Kyeongjun <Kyeongjun@DESKTOP-1O4ASCO>
2020-02-27 21:18:55 +0900

    로그아웃

--- TeamProject/src/member/account/action/LogoutProAction.java
+++ TeamProject/src/member/account/action/LogoutProAction.java
@@ -2,6 +2,7 @@ package member.account.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import action.Action;
 import vo.ActionForward;
@@ -13,8 +14,13 @@ public class LogoutProAction implements Action {
 		ActionForward forward = null;
 		System.out.println("LogoutProAction");
 		
+		HttpSession session = request.getSession();
 		
+		session.invalidate();
 		
+		forward = new ActionForward();
+		forward.setPath("");
+		forward.setRedirect(true);
 		
 		return forward;
 	}
