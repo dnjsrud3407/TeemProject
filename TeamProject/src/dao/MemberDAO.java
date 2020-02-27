72810af4429bab2cb0d519645494c96a758d2d3e
Kyeongjun <Kyeongjun@DESKTOP-1O4ASCO>
2020-02-27 14:05:36 +0900

    memberDAO 수정중

--- TeamProject/src/dao/MemberDAO.java
+++ TeamProject/src/dao/MemberDAO.java
@@ -1,6 +1,9 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import static db.JdbcUtil.*;
 
 import vo.MemberBean;
 
@@ -33,6 +36,38 @@ public MemberDAO() {}
 	public int updateMember(MemberBean member) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	public int insertMember(MemberBean member) {
+		int insertCount = 0;
+		
+		PreparedStatement pstmt = null;
+		
+		try {
+			String sql="insert into user values(?,?,?,?,?,?,?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, member.getuID());
+			pstmt.setString(2, member.getPw());
+			pstmt.setString(3, member.getU_name());
+			pstmt.setString(4, member.getAddress());
+			pstmt.setString(5, member.getPhone_num());
+			pstmt.setString(6, member.getEmail());
+			pstmt.setString(7, member.getTell_num());
+			pstmt.setString(8, member.getAddress2());
+			pstmt.setInt(9, 0);
+			pstmt.setInt(10, 0);
+			
+			insertCount = pstmt.executeUpdate();
+			System.out.println(insertCount);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		
+		
+		return insertCount;
 	}
 
 }
