ca578e084af1b5c59728a605cd0c2f422caa3196
jun <juneey30@gmail.com>
2020-04-02 14:08:03 +0900

    BoardBean, BoardDAO 합칠 건 합치고 충돌나는 부분 주석처리 해놓음

--- TeamProject/src/dao/BoardDAO.java
+++ TeamProject/src/dao/BoardDAO.java
@@ -1,6 +1,6 @@
 package dao;
 
-<<<<<<< HEAD
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -249,17 +249,6 @@ public class BoardDAO {
 		
 		return fileList;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	public int get_kID(int boardNum, String k1) {
 		int kID = -1;
@@ -613,36 +602,6 @@ public class BoardDAO {
 			return listCount;
 	}
 	
-}	
-	
-	
-
-=======
-import static db.JdbcUtil.close;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import vo.BoardBean;
-
-public class BoardDAO {
-	public BoardDAO() {}
-	
-	private static BoardDAO instance = new BoardDAO();
-	
-	public static BoardDAO getInstance() {
-		return instance;
-	}
-	
-	Connection con;
-	
-	public void setConnection(Connection con) {
-		this.con = con;
-	}
-
 	// 상품 문의 게시글 개수 가져오기
 	public int selectListCount(int kID) {
 		int listCount = 0;
@@ -666,84 +625,84 @@ public class BoardDAO {
 		return listCount;
 	}
 
-	// 상품 문의 게시글 들고오기
-	public ArrayList<BoardBean> selectList(int kID, int page, int limit) {
-		ArrayList<BoardBean> qList = new ArrayList<BoardBean>();
-		PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        // 답변 안 된 글(boardReSeq=0)이 우선적으로 보여지고
-        // 다음에는 최근글 부터 보여짐
-        String sql = "SELECT board.*, book.bookTitle FROM board"
-        		+ " JOIN book ON board.bookID = book.bookID"
-        		+ " WHERE kID=? AND boardReLev=?"
-        		+ " ORDER BY boardReSeq asc, boardReRef desc LIMIT ?,?";
-		int startRow = (page - 1) * limit;
-        try {
-			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, kID);
-			pstmt.setInt(2, 0);	// 문의글만 보기
-			pstmt.setInt(3, startRow);
-			pstmt.setInt(4, limit);
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				BoardBean board = new BoardBean(
-						rs.getInt("boardNum"), 
-						rs.getString("boardWriter"), 
-						rs.getString("boardTitle"), 
-						rs.getString("boardContent"), 
-						rs.getDate("boardRegTime"), 
-						rs.getInt("boardReRef"), 
-						rs.getInt("boardReLev"), 
-						rs.getInt("boardReSeq"), 
-						rs.getInt("bookID"), 
-						rs.getString("bookTitle")
-						);
-				qList.add(board);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-            if(rs != null) {close(rs);}
-            if(pstmt != null) {close(pstmt);}
-        }
-		
-		return qList;
-	}
+//	// 상품 문의 게시글 들고오기
+//	public ArrayList<BoardBean> selectList(int kID, int page, int limit) {
+//		ArrayList<BoardBean> qList = new ArrayList<BoardBean>();
+//		PreparedStatement pstmt = null;
+//        ResultSet rs = null;
+//        // 답변 안 된 글(boardReSeq=0)이 우선적으로 보여지고
+//        // 다음에는 최근글 부터 보여짐
+//        String sql = "SELECT board.*, book.bookTitle FROM board"
+//        		+ " JOIN book ON board.bookID = book.bookID"
+//        		+ " WHERE kID=? AND boardReLev=?"
+//        		+ " ORDER BY boardReSeq asc, boardReRef desc LIMIT ?,?";
+//		int startRow = (page - 1) * limit;
+//        try {
+//			pstmt = con.prepareStatement(sql);
+//			pstmt.setInt(1, kID);
+//			pstmt.setInt(2, 0);	// 문의글만 보기
+//			pstmt.setInt(3, startRow);
+//			pstmt.setInt(4, limit);
+//			rs = pstmt.executeQuery();
+//			while(rs.next()) {
+//				BoardBean board = new BoardBean(
+//						rs.getInt("boardNum"), 
+//						rs.getString("boardWriter"), 
+//						rs.getString("boardTitle"), 
+//						rs.getString("boardContent"), 
+//						rs.getDate("boardRegTime"), 
+//						rs.getInt("boardReRef"), 
+//						rs.getInt("boardReLev"), 
+//						rs.getInt("boardReSeq"), 
+//						rs.getInt("bookID"), 
+//						rs.getString("bookTitle")
+//						);
+//				qList.add(board);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//            if(rs != null) {close(rs);}
+//            if(pstmt != null) {close(pstmt);}
+//        }
+//		
+//		return qList;
+//	}
 
-	// 사용자 문의글 가져오기
-	public BoardBean selectBoard(int boardNum) {
-		BoardBean board = null;
-		PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        String sql = "SELECT board.*, book.bookTitle FROM board" + 
-        		" JOIN book ON board.bookID = book.bookID WHERE boardNum=?";
-        try {
-			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, boardNum);
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				board = new BoardBean(
-						boardNum, 
-						rs.getString("boardWriter"), 
-						rs.getString("boardTitle"), 
-						rs.getString("boardContent"), 
-						rs.getDate("boardRegTime"), 
-						rs.getInt("boardReRef"), 
-						rs.getInt("boardReLev"), 
-						rs.getInt("boardReSeq"), 
-						rs.getInt("bookID"), 
-						rs.getString("bookTitle")
-						);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-            if(rs != null) {close(rs);}
-            if(pstmt != null) {close(pstmt);}
-        }
-        
-		return board;
-	}
+//	// 사용자 문의글 가져오기
+//	public BoardBean selectBoard(int boardNum) {
+//		BoardBean board = null;
+//		PreparedStatement pstmt = null;
+//        ResultSet rs = null;
+//        String sql = "SELECT board.*, book.bookTitle FROM board" + 
+//        		" JOIN book ON board.bookID = book.bookID WHERE boardNum=?";
+//        try {
+//			pstmt = con.prepareStatement(sql);
+//			pstmt.setInt(1, boardNum);
+//			rs = pstmt.executeQuery();
+//			if(rs.next()) {
+//				board = new BoardBean(
+//						boardNum, 
+//						rs.getString("boardWriter"), 
+//						rs.getString("boardTitle"), 
+//						rs.getString("boardContent"), 
+//						rs.getDate("boardRegTime"), 
+//						rs.getInt("boardReRef"), 
+//						rs.getInt("boardReLev"), 
+//						rs.getInt("boardReSeq"), 
+//						rs.getInt("bookID"), 
+//						rs.getString("bookTitle")
+//						);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//            if(rs != null) {close(rs);}
+//            if(pstmt != null) {close(pstmt);}
+//        }
+//        
+//		return board;
+//	}
 
 	// 답변 등록 시 게시글 번호 구하기 (게시글 중 최대값 구하기)
 	public int selectMaxNum() {
@@ -811,4 +770,4 @@ public class BoardDAO {
 		return insertCount;
 	}
 }
->>>>>>> branch 'master' of https://github.com/dnjsrud3407/TeemProject.git
+
