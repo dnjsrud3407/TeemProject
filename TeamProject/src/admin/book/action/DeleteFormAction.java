fd2af568fd8ccc191ce04826ed75cf0ce847f9a7
itwill <itwill@DESKTOP-L65TVS8>
2020-04-09 16:31:06 +0900

    전체선택완료

--- TeamProject/src/admin/book/action/DeleteFormAction.java
+++ TeamProject/src/admin/book/action/DeleteFormAction.java
@@ -15,6 +15,10 @@ public class DeleteFormAction implements Action {
 		// list.jsp 에서 1~n개 삭제하는 경우 || detail.jsp 에서 1개 삭제하는 경우  모두 포함
 		String[] bookIDList = request.getParameterValues("bookIDList");
 		
+		for(String id : bookIDList) {
+			System.out.println(id);
+		}
+		
 		request.setAttribute("bookIDList", bookIDList);
 		
 		forward = new ActionForward();
