8f7be59dc048f3b13bac82a8042940e66507f416
itwill <itwill@DESKTOP-HH7IKIE>
2020-04-21 20:08:52 +0900

    ll

--- TeamProject/src/admin/sales/action/OrderRefundDetailAcion.java
+++ TeamProject/src/admin/sales/action/OrderRefundDetailAcion.java
@@ -1,10 +1,16 @@
 package admin.sales.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import action.Action;
+import admin.member.svc.MemberDetailService;
+import admin.sales.svc.OrderDetailService;
 import vo.ActionForward;
+import vo.MemberBean;
+import vo.OrderBean;
 
 
 public class OrderRefundDetailAcion implements Action {
@@ -21,12 +27,25 @@ public class OrderRefundDetailAcion implements Action {
 //		if(book != null) {
 //			orderRefundDetailService.plusReadcount(board_num);
 //		}
-		
+		 
 //		request.setAttribute("book", book);
 //		request.setAttribute("page", page);
 
 		ActionForward forward = null;
+		String orderNum = request.getParameter("orderNum");
 		
+		OrderDetailService orderDetailService = new OrderDetailService();
+		MemberDetailService memberDetailService = new MemberDetailService();
+		
+		List<OrderBean> order = orderDetailService.selectOrder(orderNum);
+		
+		OrderBean orderDetaile = orderDetailService.orderDetaile(orderNum);
+		MemberBean member = memberDetailService.getMember(orderDetaile.getOrder_ID());
+		
+		request.setAttribute("order", order);
+		request.setAttribute("orderDetaile", orderDetaile);
+		request.setAttribute("member", member);
+
 		forward = new ActionForward();
 		forward.setPath("./admin/order/order_refund_detail.jsp");
 		
